e68187670813372faecb8cbaadd80437725bcb5f
claire gautier <claire.gautier@edhec.com>
2021-08-18 10:11:57 +0200

    finished lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    # collection do
+    #   get :top
+    # end
+    # member do
+    #   get :chef
+    # end
+    resources :reviews, only: [ :new, :create ] # /restaurants/:restaurant_id
+  end
+  # COLLECTION: many restaurants >>>> get 'restaurants/top', to: "restaurants#top"
+  # MEMBER: details about ONE restaurant >>>>> get '/restaurants/:id/chef', to: 'restaurants#chef'
+
+  resources :reviews, only: [ :destroy ]
 end
